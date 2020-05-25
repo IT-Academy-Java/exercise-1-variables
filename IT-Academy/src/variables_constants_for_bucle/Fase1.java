0d6bdb7f25662bdc870d96940416952d2157609b
Elena Piaggio <elenapiaggio@gmail.com>
2020-05-25 13:47:58 +0200

    resolve conflict

--- IT-Academy/src/variables_constants_for_bucle/Fase1.java
+++ IT-Academy/src/variables_constants_for_bucle/Fase1.java
@@ -1,8 +1,16 @@
 package variables_constants_for_bucle;
 
-public class Test {
+public class Fase1 {
 
 	public static void main(String[] args) {
-		System.out.println("hello world!");
+		String nom = "Elena";
+		String cognom1 = "Piaggio";
+		String cognom2 = "Valdivieso";
+		int dia = 25;
+		int mes = 05;
+		int any = 2020;
+
+		System.out.println("Cognom 1: " + cognom1 + ", Cognom2: " + cognom2 + ", Nom: " + nom);
+		System.out.println(dia + " / " + mes + " / " + any);
 	}
 }
